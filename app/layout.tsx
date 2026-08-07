f912e0cc2cb5f706322f13b5a7ffb8928a2e7be8
Logan Meyer <loganrandallmeyer@gmail.com>
2026-08-07 11:53:09 -0700

    Use @vercel/analytics/next for route-change tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Cormorant_Garamond, DM_Sans } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/next'
 import Preloader from '../components/Preloader'
 import './globals.css'
 
